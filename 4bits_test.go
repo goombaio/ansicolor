a7c6d1b34c8175d71cc6865de9823ff4d20d84ee
Raül Pérez <repejota@gmail.com>
2018-09-24 03:57:10 +0200

    Adding tests

---
+++ 4bits_test.go
@@ -0,0 +1,57 @@
+// Copyright 2018, gossiper project Authors. All rights reserved.
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with this
+// work for additional information regarding copyright ownership.  The ASF
+// licenses this file to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package ansicolor_test
+
+/*
+import (
+	"fmt"
+	"testing"
+
+	"github.com/goombaio/ansicolor"
+)
+
+func TestSGR(t *testing.T) {
+	str := "This is a black string on white background"
+	result := ansicolor.SGR(str, 30, 47)
+	fmt.Println(result)
+}
+
+func TestSGR_foreground(t *testing.T) {
+	str := "This is a green string"
+	result := ansicolor.SGR(str, 32)
+	fmt.Println(result)
+}
+
+func TestSGR_foreground_bright(t *testing.T) {
+	str := "This is a bright green string"
+	result := ansicolor.SGR(str, 1, 32)
+	fmt.Println(result)
+}
+
+func TestSGR_background(t *testing.T) {
+	str := "This is a string on red background"
+	result := ansicolor.SGR(str, 41)
+	fmt.Println(result)
+}
+
+func TestSGR_background_bright(t *testing.T) {
+	str := "This is a string on bright red background"
+	result := ansicolor.SGR(str, 1, 41)
+	fmt.Println(result)
+}
+*/
---
+++ color_test.go
@@ -0,0 +1,58 @@
+// Copyright 2018, gossiper project Authors. All rights reserved.
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with this
+// work for additional information regarding copyright ownership.  The ASF
+// licenses this file to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package ansicolor_test
+
+import (
+	"testing"
+
+	"github.com/goombaio/ansicolor"
+)
+
+func TestColor_DisableColor(t *testing.T) {
+	ansicolor.DisableColor()
+
+	if ansicolor.Colorifier.IsColorEnabled() != false {
+		t.Fatalf("Expected to be false but it does not")
+	}
+
+	if ansicolor.Colorifier.IsColorDisabled() != true {
+		t.Fatalf("Expected to be true but it does not")
+	}
+}
+
+func TestColor_EnabledColor(t *testing.T) {
+	ansicolor.EnableColor()
+
+	if ansicolor.Colorifier.IsColorEnabled() != true {
+		t.Fatalf("Expected to be true but it does not")
+	}
+
+	if ansicolor.Colorifier.IsColorDisabled() != false {
+		t.Fatalf("Expected to be false but it does not")
+	}
+}
+
+func TestColor_Color(t *testing.T) {
+	str := "This is an string"
+
+	result := ansicolor.Color(str)
+
+	if result != str {
+		t.Fatalf("result expected %q but got %q", str, result)
+	}
+}
